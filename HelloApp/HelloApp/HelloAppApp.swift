64ad722c83c357c2492fd562557d7c5462c59c30
Ossamoon <osamu950623@gmail.com>
2020-10-28 19:03:55 +0900

    Xcodeでプロジェクトを立ち上げた

---
+++ HelloApp/HelloApp/HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppApp.swift
+//  HelloApp
+//
+//  Created by 齋藤修 on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
